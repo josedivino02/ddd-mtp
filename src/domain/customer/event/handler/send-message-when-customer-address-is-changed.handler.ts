17d46212083bf06d6e2d6ad0170f3f2791c80475
José Divino <josedivinooficial02@gmail.com>
2025-08-26 23:07:35 -0300

    implementando módulos

---
+++ src/domain/customer/event/handler/send-message-when-customer-address-is-changed.handler.ts
@@ -0,0 +1,11 @@
+import EventHandlerInterface from '../../../@shared/event/event-handler.interface';
+import CustomerAddressChangedEvent from '../customer-address-changed.event';
+
+export default class SendMessageWhenCustomerAddressIsChangedHandler implements EventHandlerInterface<CustomerAddressChangedEvent> {
+  handle(event: CustomerAddressChangedEvent): void {
+    const { id, nome, endereco } = event.eventData;
+
+    console.log(`Endereço do cliente: ${id}, ${nome} alterado para: ${endereco}`);
+  }
+
+}
